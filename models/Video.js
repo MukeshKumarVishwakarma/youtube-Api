02f8452610af6906d1f71c6af5df950d8d055bd7
mukesh <mukeshvishwakarma251003@gmail.com>
2024-11-23 11:09:29 +0530

    updated video model

--- models/Video.js
+++ models/Video.js
@@ -11,8 +11,9 @@ const videoSchema = new mongoose.Schema({
         required: true
     },
     user_id: {
-        type: String,
-        required: true
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     },
     videoUrl: {
         type: String,
